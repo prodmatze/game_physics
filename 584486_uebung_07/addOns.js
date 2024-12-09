0805b6fc668c302cfdbcb5a45959a292d2f34914
matze <104998443+prodmatze@users.noreply.github.com>
2024-12-09 15:56:37 +0100

    started on implementing drag, fucked myself by using cm instead of m at the start of project

---
+++ 584486_uebung_07/addOns.js
@@ -0,0 +1,104 @@
+/* template GTAT2 Game Technology & Interactive Systems */
+/* Autor: Mathieu Wassmuth  */
+/* Übung Nr.7 */
+/* Datum: 09.12.2024 */
+
+/* draw red rectangle */
+function drawRedRectangle(x, y, w, h, c) {
+	fill(c);
+	rectMode(CORNER);
+	rect(x, y, w, h);
+}
+
+function drawRectangle(x, y, w, h, c) {
+	fill(c);
+	noStroke();
+	rectMode(CORNER);
+	rect(x, y, w, h);
+}
+
+function draw_flagpole() {
+	fill("#0000FF");
+	noStroke();
+	beginShape();
+	vertex(0 + metric.length * scale, 0 + canvasHeight - 10);
+	vertex(100, 50);
+	vertex(100, 200);
+
+	endShape();
+}
+
+function drawTriangle(x1, y1, x2, y2, x3, y3, c) {
+	fill(c);
+	noStroke();
+	triangle(x1, y1, x2, y2, x3, y3);
+}
+
+function drawFlag(x1, y1, x2, y2, x3, y3, c, s) {
+	fill(c);
+	stroke(s);
+	triangle(x1, y1, x2, y2, x3, y3);
+}
+
+
+function draw_circle(x, y, d, c) {
+	fill(c);
+	noStroke();
+	circle(x, y, d);
+}
+
+function scale_canvas(canvas_width, canvas_height, scale_factor) {
+	const new_canvas_width = canvas_width / scale_factor;
+	const new_canvas_height = canvas_height / scale_factor;
+
+	return new_canvas_width, new_canvas_height;
+}
+
+let obstacle = {
+	x: - metric.right_rect_width / 2,
+	y: metric.height,
+	height: 100,
+	width: 15,
+}
+
+function reposition_obstacle(obstacle_at_start) {
+	if (obstacle_at_start) {
+		obstacle.x = - 770;
+	} else {
+		obstacle.x = -metric.right_rect_width / 2;
+	}
+}
+
+function draw_scene() {
+	//right rect
+	drawRectangle(-metric.right_rect_width, 0, metric.right_rect_width, metric.height, '#0000ff');
+
+	//left rect
+	drawRectangle(-metric.right_rect_width - metric.left_rect_width - metric.hole_width, 0, metric.left_rect_width, metric.height, '#0000ff');
+
+	//hole rec
+	drawRectangle(-metric.right_rect_width - metric.hole_width, 0, metric.hole_width, metric.hole_height, "#0000ff");
+
+	//schortstein
+	drawRectangle(-metric.right_rect_width - metric.left_rect_width - metric.hole_width, 0, metric.schornstein_width, metric.schornstein_height, "#0000ff");
+
+	//red obstacle
+	drawRectangle(obstacle.x, obstacle.y, obstacle.width, obstacle.height, "#ff0000");
+
+	//blue triangle
+	drawTriangle(triangle_coords.x1, triangle_coords.y1, triangle_coords.x2, triangle_coords.y2, triangle_coords.x3, triangle_coords.y3, "#0000ff");
+
+	//flagpole
+	drawRectangle(flag_pole_coords.x1, flag_pole_coords.y1, 5, metric.flagpole_height, "#000000");
+	//flag
+	drawFlag(flag_coords.x1, flag_coords.y1, flag_coords.x2, flag_coords.y2, flag_coords.x3, flag_coords.y3, ("#ffff00"), 1);
+
+	//slingshot
+	drawTriangle(slingshot.x1, slingshot.y1, slingshot.x2, slingshot.y2, slingshot.x3, slingshot.y3, "#00ff00");
+
+	//red ball
+	draw_circle(red_ball_x, red_ball_y, red_ball_d, "#ff0000");
+
+	//playball
+	draw_circle(ball_x, ball_y, ball_d, "#0000ff");
+}
---
+++ 584486_uebung_07/drag_and_wind.js
@@ -0,0 +1,33 @@
+/* template GTAT2 Game Technology & Interactive Systems */
+/* Autor: Mathieu Wassmuth  */
+/* Übung Nr.7 */
+/* Datum: 09.12.2024 */
+
+function calculate_drag(ball_velocity_x, ball_velocity_y, c_w, density_air, ball_mass, cross_section_a) {
+
+  //insgesamtgeschwindigkeit berechnen
+  let v = Math.sqrt(ball_velocity_x * ball_velocity_x + ball_velocity_y * ball_velocity_y);
+
+  if (v === 0) {
+    return { ax: 0, ay: 0 };
+  }
+
+  //reibungskraft berechnen
+  let drag_force = 0.5 * c_w * density_air * cross_section_a * v * v;
+
+  //einheitsvektoren entgegengesetzt zur bewegung
+  let ball_velocity_x_normal = ball_velocity_x / v;
+  let ball_velocity_y_normal = ball_velocity_y / v;
+
+  //reibungskraft für jede richtung
+  let drag_force_x = - drag_force * ball_velocity_x_normal;
+  let drag_force_y = - drag_force * ball_velocity_y_normal;
+
+  //neue beschleunigung ausrechnen
+  let acceleration_x = drag_force_x / ball_mass;
+  let acceleration_y = drag_force_y / ball_mass;
+
+  return { ax: acceleration_x, ay: acceleration_y };
+
+}
+
